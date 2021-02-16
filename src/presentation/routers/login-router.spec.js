7991e4ac5cfc708d447e268337efc801e38fbb7d
Zackynson <crys.chb@hotmail.com>
2021-02-16 07:33:45 -0300

    ensure login router returns statuscode 400 if no email is provided

---
+++ src/presentation/routers/login-router.spec.js
@@ -0,0 +1,23 @@
+class LoginRouter {
+  async route (httpRequest) {
+    if (!httpRequest.body.email) {
+      return { statusCode: 400 }
+    }
+  }
+}
+
+describe('login-router', () => {
+  test('Should return 400 if no email is provided', async () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body:
+      {
+        password: 'any_password'
+      }
+    }
+
+    const httpResponse = await sut.route(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
